e317433b103efa61aa29c383d093333f5ba6ec13
Student Apna College <dhanushnuthichenchu@gmail.com>
2025-08-21 14:20:48 +0530

    Episode-03(foundations of React & Babel) is completed

--- Entire/App.js
+++ Entire/App.js
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement() => creates React Element(Object) => root.render => converts it to HTML(Browser Understands)
+const ele = <span>ReactElement</span>;
 
-const heading = React.createElement("h1",{id: "parent"},"ABD17")
+const titleElement = (
+  <h1>
+    {ele}
+    {" " + 17}
+    Title Element Using Jsx...!!
+  </h1>
+);
+
+const TitleComponent = () => (
+  <div className="title" tabIndex="3">
+    {titleElement}
+    TitleComponent using JSX(component Composition)...!!
+  </div>
+);
+
+// React Functional Component
+// Component Composition
+const HeadingComponent = () => (
+  <div id="parent">
+    <TitleComponent />
+    {/* 2) <TitleComponent>
+    </TitleComponent> 
+    3) {TitleComponent()}
+    */}
+    <h1 className="heading">Namate React Using Functional Component..!!</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
